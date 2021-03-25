d1af1be2ed1b031701aa44aca6650a39e0cd65ca
Nate Kent <ngkent75@gmail.com>
2021-03-24 23:03:08 -0400

    filled in models files

---
+++ models/Blog.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+  }
+);
+
+module.exports = Blog;
---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Blog = require('./Blog');
+
+User.hasmany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog };
