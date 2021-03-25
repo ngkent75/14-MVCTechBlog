9bce452df4d41275b668a65edb4630010c50273b
Nate Kent <ngkent75@gmail.com>
2021-03-25 02:29:33 -0400

    filled out public files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 
     // pass into template
     res.render('homepage', {
-      projects,
+      blogs,
       logged_in: req.session.logged_in
     });
   } catch (err) {
